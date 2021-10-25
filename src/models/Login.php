ba9ea9d69c18d24d1dba2f581e9480ac27fa7476
Ricardo Ferreira <ricardo.lazafer@gmail.com>
2021-10-24 21:37:19 -0300

    Commit - Tratamento das mesnagens de erros dos campos Login e senha

--- src/models/Login.php
+++ src/models/Login.php
@@ -2,8 +2,26 @@
 loadModel('User');
 
 class Login extends Model {
+
+    public function validate() {
+        $errors = [];
+
+        if(!$this->email) {
+            $errors['email'] = 'E-mail é um campo obrigatório.';
+        }
+
+        if(!$this->password) {
+            $errors['password'] = 'Por favor, informe a senha.';
+        }
+
+        if(count($errors) > 0) {
+            throw new ValidationException($errors);
+        }
+    }
+
     // Criação de Função para checar se o usuário está logado ou não
     public function checkLogin() {
+        $this->validate();
         $user = User::getOne(['email' => $this->email]);
         if($user) {
             if($user->end_date) {
