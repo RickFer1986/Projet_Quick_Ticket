baac0fc795e3c6b60b5fd81dac92410929c53304
Ricardo Ferreira <ricardo.lazafer@gmail.com>
2021-10-25 22:50:46 -0300

    Commit - Front-End: Criação do cabeçalho

---
+++ src/views/template/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/comum.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/icofont.min.css">
+    <link rel="stylesheet" href="assets/css/template.css">
+    <title>Quick Ticket</title>
+</head>
+
+<body>
+    <header class="header">
+        <div class="logo">
+            <span class="font-weigth-light">Quick </span>
+            <i class="icofont-ticket mx-2"></i>
+            <span class="font-weigth-light">Ticket</span>
+        </div>
+        <div class="menu-toggle mx-3">
+            <i class="icofont-navigation-menu"></i>
+        </div>
+        <div class="spacer"></div>
+    </header>
