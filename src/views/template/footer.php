3f7714321f1411fdd426fcd1de5b8e1c08902589
Ricardo Ferreira <ricardo.lazafer@gmail.com>
2021-10-27 23:01:18 -0300

    Commit - Front-End: Criação do Main (estilização do CSS)

--- src/views/template/footer.php
+++ src/views/template/footer.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <span>Desenvolvido com</span>
     <span><i class="icofont-heart text-danger mx-1"></i></span>
-    <span>por COD<span class="text-danger">3</span>R</span>
+    <span>por <span class="text-danger">D</span>ev<span class="text-danger">S</span>ystem</span>
 </footer>
 <script src="assets/js/app.js"></script>
 </body>
